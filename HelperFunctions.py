927b092ad212310f59dd631d7402abec71abfa88
Luke Taverne <luketaverne@gmail.com>
2015-03-12 12:24:01 -0400

    added HelperFunctions.py. Contains a function to test if a given external program exists

---
+++ HelperFunctions.py
@@ -0,0 +1,20 @@
+# A bunch of helpful mini functions. Putting them here to avoid
+# clutter in the main program.
+
+def which(program):
+    import os
+    def is_exe(fpath):
+        return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
+
+    fpath, fname = os.path.split(program)
+    if fpath:
+        if is_exe(program):
+            return program
+    else:
+        for path in os.environ["PATH"].split(os.pathsep):
+            path = path.strip('"')
+            exe_file = os.path.join(path, program)
+            if is_exe(exe_file):
+                return exe_file
+
+    return None
